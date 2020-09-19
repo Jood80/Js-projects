999b5dbd31c91020dcf83f935d871766f4bd180c
Nujood <shawar.nujood@gmail.com>
2020-09-19 18:10:46 +0300

    fixing buttton tag naming and style

--- 4-popup/app.js
+++ 4-popup/app.js
@@ -2,12 +2,12 @@ const open = document.getElementById("open");
 
 const close = document.getElementById("close");
 
-const conatiner = document.getElementById("container");
+const container = document.getElementById("container");
 
 open.addEventListener("click", () => {
-  conatiner.classList.add("active");
+  container.classList.add("active");
 });
 
 close.addEventListener("click", () => {
-  conatiner.classList.remove("active");
+  container.classList.remove("active");
 });
